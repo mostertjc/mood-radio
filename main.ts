62e8deae67a0b283d78e083443ae1f440a619eac
mostertjc <102650119+mostertjc@users.noreply.github.com>
2022-12-22 04:54:41 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(1)
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+})
